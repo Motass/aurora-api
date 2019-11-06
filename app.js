357cbfc6c2efe8f95f76db9bbbb09ca382b41c97
Thomas <thomas@fedfedfed.com>
2019-11-05 16:46:12 -0800

    use cors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var logger = require('morgan');
 var mongoose = require('mongoose');
 var passport = require('passport');
+const cors = require('cors');
 
 var path = require('path');
 var dotEnvPath = path.resolve('../.env.test');
@@ -21,6 +22,7 @@ mongoose.connect(process.env.MONGO_URI,
 
 var app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
